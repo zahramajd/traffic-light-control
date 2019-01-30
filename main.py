9e1539b4747e49e9fe86129a3ad6eace1f587398
zahramajd <zahra.majdabadi95@gmail.com>
2019-01-31 00:41:04 +0330

    minor change

--- main.py
+++ main.py
@@ -21,86 +21,76 @@ def convert_actions(phases):
     actions = pd.DataFrame(columns=['A'])
     
     for index, row in phases.iterrows():
-        f = False
 
         if(np.array_equal([33, 33, 13, 13],row.values)):
             actions.loc[index] = [0]
-            f = True
 
         if(np.array_equal([33, 13, 33, 13],row.values)):
             actions.loc[index] = [1]
-            f = True
 
         if(np.array_equal([33, 13, 13, 33],row.values)):
             actions.loc[index] = [2]
-            f = True
 
         if(np.array_equal([13, 33, 33, 13],row.values)):
             actions.loc[index] = [3]
-            f = True
 
         if(np.array_equal([13, 33, 13, 33],row.values)):
             actions.loc[index] = [4]
-            f = True
 
         if(np.array_equal([13, 13, 33, 33],row.values)):
             actions.loc[index] = [5]
-            f = True
 
         if(np.array_equal([33, 23, 23, 13],row.values)):
             actions.loc[index] = [6]
-            f = True
 
         if(np.array_equal([33, 23, 13, 23],row.values)):
             actions.loc[index] = [7]
-            f = True
 
         if(np.array_equal([33, 13, 23, 23],row.values)):
             actions.loc[index] = [8]
-            f = True
 
         if(np.array_equal([23, 33, 23, 13],row.values)):
             actions.loc[index] = [9]
-            f = True
 
         if(np.array_equal([23, 33, 13, 23],row.values)):
             actions.loc[index] = [10]
-            f = True
 
         if(np.array_equal([13, 33, 23, 23],row.values)):
             actions.loc[index] = [11]
-            f = True
 
         if(np.array_equal([23, 23, 33, 13],row.values)):
             actions.loc[index] = [12]
-            f = True
 
         if(np.array_equal([23, 13, 33, 23],row.values)):
             actions.loc[index] = [13]
-            f = True
 
         if(np.array_equal([13, 23, 33, 23],row.values)):
             actions.loc[index] = [14]
-            f = True
 
         if(np.array_equal([23, 23, 13, 33],row.values)):
             actions.loc[index] = [15]
-            f = True
 
         if(np.array_equal([23, 13, 23, 33],row.values)):
             actions.loc[index] = [16]
-            f = True
 
         if(np.array_equal([13, 23, 23, 33],row.values)):
             actions.loc[index] = [17]
-            f = True
 
         if(np.array_equal([23, 23, 23, 23],row.values)):
             actions.loc[index] = [18]
-            f = True
 
-        if(f == False):
+        if(np.array_equal([53, 13, 13, 13],row.values)):
             actions.loc[index] = [19]
+
+        if(np.array_equal([13, 53, 13, 13],row.values)):
+            actions.loc[index] = [20]
+
+        if(np.array_equal([13, 13, 53, 13],row.values)):
+            actions.loc[index] = [21]
+
+        if(np.array_equal([13, 13, 13, 53],row.values)):
+            actions.loc[index] = [22]
+
 
     return actions
 
