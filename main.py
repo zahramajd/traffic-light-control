444f01ff06a49e93f0de86d877c761ccd635d549
zahramajd <zahra.majdabadi95@gmail.com>
2019-02-02 20:17:12 +0330

    handle timesteps

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tensorflow.contrib import rnn
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
@@ -7,7 +6,8 @@ from matplotlib import pyplot
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
-
+from pandas import DataFrame
+from pandas import concat
 
 def load_data():
     data = pd.read_csv("./DataSet-1/dataset536.csv")
@@ -116,31 +116,90 @@ raw_info, states, phases = load_data()
 actions = convert_actions(phases)
 info = normalize(raw_info)
 
+# info_train, info_test = split_data(info)
+# states_train, states_test = split_data(states)
+# actions_train, actions_test = split_data(actions)
 
-info_train, info_test = split_data(info)
-states_train, states_test = split_data(states)
-actions_train, actions_test = split_data(actions)
+
+def state_state_1_timestep():
+    train_X = info_train[:, :-1]
+    test_X = info_test[:, :-1]
+
+    train_y = states_train
+    test_y = states_test
+
+    train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+    test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+
+    # design network
+    model = Sequential()
+    model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+    model.add(Dense(1))
+    model.compile(loss='mae', optimizer='adam')
+    # fit network
+    history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+    # plot history
+    pyplot.plot(history.history['loss'], label='train')
+    pyplot.plot(history.history['val_loss'], label='test')
+    pyplot.legend()
+    pyplot.show()
+
+def state_state_n_timestep(info, states):
+
+    timesteps = 10
+    n_features = 8
+
+    n_obs = timesteps * n_features
+
+    data = DataFrame(info)
+    label = DataFrame(states)
+
+    cols = []
+
+    for i in range(timesteps, 0, -1):
+        cols.append(data.shift(i))
 
 
-train_X = info_train[:, :-1]
-test_X = info_test[:, :-1]
+    for i in range(0,1):
+        cols.append(label.shift(-i))
 
-train_y = states_train
-test_y = states_test
+    data = concat(cols, axis=1)
+    data.dropna(inplace=True)
 
-train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+    data = data.values
 
-# design network
-model = Sequential()
-model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
-model.add(Dense(1))
-model.compile(loss='mae', optimizer='adam')
-# fit network
-history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
-# plot history
-pyplot.plot(history.history['loss'], label='train')
-pyplot.plot(history.history['val_loss'], label='test')
-pyplot.legend()
-pyplot.show()
+    train_percent = 0.9
+
+    m = len(data)
+    point = int(train_percent * m) 
+
+    train = data[:point, :]
+    test = data[point:, :]
+
+    train_X = train[:, :n_obs]
+    test_X = test[:, :n_obs]
+
+    train_y = train[:, -1]
+    test_y = test[:, -1]
+
+    # reshape input to be 3D [samples, timesteps, features]
+    train_X = train_X.reshape((train_X.shape[0], timesteps, n_features))
+    test_X = test_X.reshape((test_X.shape[0], timesteps, n_features))
+
+    # design network
+    model = Sequential()
+    model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+    model.add(Dense(1))
+    model.compile(loss='mae', optimizer='adam')
+    # fit network
+    history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+    # plot history
+    pyplot.plot(history.history['loss'], label='train')
+    pyplot.plot(history.history['val_loss'], label='test')
+    pyplot.legend()
+    pyplot.show()
+
+    return
+
+
+state_state_n_timestep(info, states)
